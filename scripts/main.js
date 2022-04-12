458df7705d1d0929ba4289e6ae567ed497c20fab
Shauri Maigua <90189750+SCNMC@users.noreply.github.com>
2022-04-12 13:02:10 +0200

    click events

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+const buttons= document.querySelectorAll("a")
+
+
+buttons.forEach(button=>{
+
+    button.addEventListener('click', onClick)
+    button.addEventListener('dblclick', onDblClick)
+    button.addEventListener('mousedown', onMouseDown)
+    button.addEventListener('mouseup', onMouseUp)
+  
+  })
+  
+  function onClick() {
+    this.classList.toggle('blue')
+  }
+  
+  function onDblClick() {
+    this.classList.toggle('yellow')
+  }
+  
+  function onMouseDown() {
+    const self = this
+    timer = setTimeout(()=>{
+      self.classList.toggle('green')
+    }, 500)
+  }
+  
+  function onMouseUp() {
+    clearTimeout(timer)
+  }
